108e09393084dd6ed98d3f4850a76078722c6aac
justin-chen <justin.uob@gmail.com>
2017-12-14 15:36:25 +0800

    符合複合pattern, 指定某一類別下所有方法

--- src/tw/com/nec/justin_chen/aspect/LoggingAspect.java
+++ src/tw/com/nec/justin_chen/aspect/LoggingAspect.java
@@ -8,8 +8,8 @@ import org.aspectj.lang.annotation.Pointcut;
 @Aspect
 public class LoggingAspect {
 
-	// Advice
-	@Before("allGetters()")
+	// Advice (同時符合這兩個patterns，才執行)
+	@Before("allGetters() && allCircleMethods()")
 	public void LoggingAdvice(){
 		System.out.println("LoggingAspect.LoggingAdvice() called");
 	}
@@ -19,7 +19,15 @@ public class LoggingAspect {
 		System.out.println("second advice called");
 	}
 	
+	
+	// 以下定義pattern...
 	@Pointcut("execution(public * get*())")
 	public void allGetters(){}
+	
+	// 執行Circle下的所有方法的pattern
+	//@Pointcut("execution(* * tw.com.nec.justin_chen.model.Circle*())")
+	@Pointcut("within(tw.com.nec.justin_chen.model.Circle)") // 第二種寫法
+	public void allCircleMethods(){}
+	
 	
 }
