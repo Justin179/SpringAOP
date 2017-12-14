ce985c10abc9b2ae7698f6a7f9294fc392f4b5e7
justin-chen <justin.uob@gmail.com>
2017-12-14 15:04:03 +0800

    @Pointcut定義pattern給多個advice參考

--- src/tw/com/nec/justin_chen/AopMain.java
+++ src/tw/com/nec/justin_chen/AopMain.java
@@ -14,7 +14,7 @@ public class AopMain {
 //		ApplicationContext ctx = new ClassPathXmlApplicationContext("spring.xml");
 		try (ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("spring.xml")) {
 			ShapeService shapeService = ctx.getBean("shapeService",ShapeService.class);
-			System.out.println(shapeService.getCircle().getName());
+			System.out.println(shapeService.getTriangle().getName());
 		}
 
 		
--- src/tw/com/nec/justin_chen/aspect/LoggingAspect.java
+++ src/tw/com/nec/justin_chen/aspect/LoggingAspect.java
@@ -2,15 +2,24 @@ package tw.com.nec.justin_chen.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 
 
 @Aspect
 public class LoggingAspect {
 
 	// Advice
-	@Before("execution(public * get*())")
+	@Before("allGetters()")
 	public void LoggingAdvice(){
 		System.out.println("LoggingAspect.LoggingAdvice() called");
 	}
+	
+	@Before("allGetters()")
+	public void secondAdvice(){
+		System.out.println("second advice called");
+	}
+	
+	@Pointcut("execution(public * get*())")
+	public void allGetters(){}
 	
 }
